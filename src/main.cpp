42072c948cc492a5e10e61fd53aa78938cc4abca
Ar-D3s <archied3smond@gmail.com>
2025-11-04 23:46:02 +0000

    Completed player spritesheet, rendering player

--- src/main.cpp
+++ src/main.cpp
@@ -3,16 +3,26 @@
 
 int main() {
 
+    // Initial Window Dimensions
     unsigned int windowHeight = 600.f;
     unsigned int windowWidth = 800.f;
 
+    // Renders window and sets view to size of window
     sf::RenderWindow window(sf::VideoMode({windowWidth, windowHeight}), "My game");
     sf::View view;
     view.setCenter({0.f, 0.f});
-    view.setSize({800.f, 600.f});
+    view.setSize({static_cast<float>(windowWidth), static_cast<float>(windowHeight)});
 
+    // Creates player and its texture
+    sf::Texture playerSheet("textures/Player.png");
+    sf::Sprite player(playerSheet);
+    player.setTextureRect(sf::IntRect({0, 0}, {16, 16}));
+    player.setScale({4.f, 4.f});
+
+    // Game loop
     while (window.isOpen()) {
 
+        // Resets the view to the size of the window so stuff doesn't stretch when resizing
         while (const std::optional event = window.pollEvent()) {
 
             if (event->is<sf::Event::Closed>()) {
@@ -31,6 +41,7 @@ int main() {
 
         window.clear({34, 73, 93});
         window.setView(view);
+        window.draw(player);
         window.display();
     }
 
