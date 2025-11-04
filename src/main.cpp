734ea48ee6c725a36c6b54e905215151dd218f7a
Ar-D3s <archied3smond@gmail.com>
2025-11-04 18:52:07 +0000

    Set up basic SFML functionality

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main() {
-    std::cout << "hello" << std::endl;
+
+    unsigned int windowHeight = 600.f;
+    unsigned int windowWidth = 800.f;
+
+    sf::RenderWindow window(sf::VideoMode({windowWidth, windowHeight}), "My game");
+    sf::View view;
+    view.setCenter({0.f, 0.f});
+    view.setSize({800.f, 600.f});
+
+    while (window.isOpen()) {
+
+        while (const std::optional event = window.pollEvent()) {
+
+            if (event->is<sf::Event::Closed>()) {
+                window.close();
+            }
+
+            else if (const auto* resize = event->getIf<sf::Event::Resized>()) {
+                windowWidth = resize->size.x;
+                windowHeight = resize->size.y;
+
+                view.setSize({static_cast<float>(windowWidth), static_cast<float>(windowHeight)});
+                window.setView(view);
+            }
+
+        }
+
+        window.clear({34, 73, 93});
+        window.setView(view);
+        window.display();
+    }
+
     return 0;
 }
