608c1d709e8fcf51886a89dc427cfdb3bbf68b98
Ar-D3s <archied3smond@gmail.com>
2025-11-06 00:30:38 +0000

    Downwards player animation

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,26 @@
 int main() {
 
     // Initial Window Dimensions
-    unsigned int windowHeight = 600.f;
-    unsigned int windowWidth = 800.f;
+    unsigned int windowHeight = 600;
+    unsigned int windowWidth = 800;
 
-    float playerTileSize = 16.f;
-    int frameSpeed = 5;
-    int frameCount = 0;
-    int animationFrameCount = 1;
+    // Creates player and its texture
+    sf::Texture playerSheet("textures/Player.png");
+    sf::Sprite player(playerSheet);
+    player.setScale({4.f, 4.f});
+
+    // Player Animation Variables
+    int frameHeight = 16;
+    int frameWidth = 16;
+
+    float frameDuration = 0.15f;
+    sf::Clock clock;
+
+    std::vector<int> walkForwards = {0, 1, 0, 2};
+    std::vector<int> walkBackwards = {3, 4, 3, 5};
+    int currentIndex = 0;
+    sf::IntRect frameRect({0, 0}, {frameWidth, frameHeight});
+    player.setTextureRect(frameRect);
 
     // Renders window and sets view to size of window
     sf::RenderWindow window(sf::VideoMode({windowWidth, windowHeight}), "My game");
@@ -19,13 +32,7 @@ int main() {
     sf::View view;
     view.setCenter({0.f, 0.f});
     view.setSize({static_cast<float>(windowWidth), static_cast<float>(windowHeight)});
-
-    // Creates player and its texture
-    sf::Texture playerSheet("textures/Player.png");
-    sf::Sprite player(playerSheet);
-    player.setTextureRect(sf::IntRect({0, 0}, {16, 16}));
-    player.setScale({4.f, 4.f});
-
+ 
     // Game loop
     while (window.isOpen()) {
 
@@ -49,12 +56,26 @@ int main() {
         window.clear({34, 73, 93});
         window.setView(view);
 
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)) player.move({0.f, -5.f});
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S)) player.move({0.f, 5.f});
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)) player.move({-5.f, 0.f});
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D)) player.move({5.f, 0.f});
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)) player.move({0.f, -3.f});
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D)) player.move({3.f, 0.f});
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)) player.move({-3.f, 0.f});
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S)) {
+    
+            player.move({0.f, 3.f});
+            if (clock.getElapsedTime().asSeconds() > frameDuration) {
 
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::R)) player.setTextureRect(sf::IntRect({16, 0}, {32, 16}));
+                currentIndex = (currentIndex + 1) % walkForwards.size();
+                int currentFrame = walkForwards[currentIndex];
+                frameRect.position.x = currentFrame * frameWidth;
+                frameRect.position.y = 0;
+                frameRect.size.y = frameHeight;
+                frameRect.size.x = frameWidth;
+                player.setTextureRect(frameRect);
+                clock.restart();
+
+        }
+
+        }   
 
         window.draw(player);
         window.display();
