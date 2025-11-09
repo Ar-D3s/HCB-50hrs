4ba9d94703bf2921e67ca0d19b4c73e52f65d7db
Ar-D3s <archied3smond@gmail.com>
2025-11-09 23:53:48 +0000

    nothing

--- src/tileTypes.cpp
+++ src/tileTypes.cpp
@@ -4,7 +4,7 @@
 #include <vector>
 
 std::vector<tileType> getTileTypes() {
-    return std::vector<tileType> {
+    return {
         {"Grass", false, sf::IntRect(sf::Vector2i(0, 0), sf::Vector2i(16, 16))},
         {"Flowers", false, sf::IntRect(sf::Vector2i(16, 0), sf::Vector2i(16, 16))},
         {"Water", true, sf::IntRect(sf::Vector2i(32, 0), sf::Vector2i(16, 16))}
