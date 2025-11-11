42493245fa4c52192a6cf2e84fca6fc96acfc588
Ar-D3s <archied3smond@gmail.com>
2025-11-11 21:23:18 +0000

    Debugged tilemap, works now!!

--- src/tileMap.cpp
+++ src/tileMap.cpp
@@ -18,41 +18,47 @@ Tilemap::Tilemap(std::vector<int>& tilemap, int mapWidth, int mapHeight)
 
     resize(6 * tilesWide * tilesHigh);
 
+    // Lets the numbers in tiles list be accessed in order
+    int textureIndex = 0;
+
+    // Cycles through each column
     for (int tileH = 0; tileH < tilesHigh; tileH ++) {
 
-        int counter = tileH * tilesWide * 6;
+        // Keeps track of the elements in the vertex array
+        int yIndex = tileH * tilesWide * 6;
 
+        // Cycles through each row
         for (int tileW = 0; tileW < tilesWide; tileW ++) {
 
-            int realCounter = counter + (tileW * 6);
+            // 
+            int xIndex = yIndex + (tileW * 6);
 
-            float indexX = tileW * TILESIZE;
-            float indexY = tileH * TILESIZE;
+            float xPos = tileW * TILESIZE;
+            float yPos = tileH * TILESIZE;
 
-            sf::Vector2f v0 = {indexX, indexY};
-            sf::Vector2f v1 = {indexX + TILESIZE, indexY};
-            sf::Vector2f v2 = {indexX, indexY + TILESIZE};
-            sf::Vector2f v3 = {indexX + TILESIZE, indexY + TILESIZE};
+            sf::Vector2f v0 = {xPos, yPos};
+            sf::Vector2f v1 = {xPos + TILESIZE, yPos};
+            sf::Vector2f v2 = {xPos, yPos + TILESIZE};
+            sf::Vector2f v3 = {xPos + TILESIZE, yPos + TILESIZE};
 
-            (*this)[realCounter].position = v0;
-            (*this)[realCounter + 1].position = v1;
-            (*this)[realCounter + 2].position = v2;
-            (*this)[realCounter + 3].position = v3;
-            (*this)[realCounter + 4].position = v2;
-            (*this)[realCounter + 5].position = v1;  
+            (*this)[xIndex].position = v0;
+            (*this)[xIndex + 1].position = v1;
+            (*this)[xIndex + 2].position = v2;
+            (*this)[xIndex + 3].position = v3;
+            (*this)[xIndex + 4].position = v2;
+            (*this)[xIndex + 5].position = v1;  
+
+            int tilePos = tiles[textureIndex];
+
+            (*this)[xIndex].texCoords = sf::Vector2f(16.f * tilePos, 0.f);            
+            (*this)[xIndex + 1].texCoords = sf::Vector2f(16.f * tilePos + 16, 0.f);
+            (*this)[xIndex + 2].texCoords = sf::Vector2f(16.f * tilePos, 16.f);
+            (*this)[xIndex + 3].texCoords = sf::Vector2f(16.f * tilePos + 16, 16.f);
+            (*this)[xIndex + 4].texCoords = sf::Vector2f(16.f * tilePos, 16.f);
+            (*this)[xIndex + 5].texCoords = sf::Vector2f(16.f * tilePos + 16, 0.f);
+
+            textureIndex ++;
+
         }
-    
     }
-
-    int vertexCount = 0;
-    for (int i : tiles) {
-        (*this)[vertexCount].texCoords = sf::Vector2f(16.f * i, 0.f);            
-        (*this)[vertexCount + 1].texCoords = sf::Vector2f(16.f * i + 16, 0.f);
-        (*this)[vertexCount + 2].texCoords = sf::Vector2f(16.f * i, 16.f);
-        (*this)[vertexCount + 3].texCoords = sf::Vector2f(16.f * i + 16, 16.f);
-        (*this)[vertexCount + 4].texCoords = sf::Vector2f(16.f * i, 16.f);
-        (*this)[vertexCount + 5].texCoords = sf::Vector2f(16.f * i + 16, 0.f);
-        vertexCount += 6;
-    }
-
 }
